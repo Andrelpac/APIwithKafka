a0034d55ea4359bd50ad03f8803b1e6683ca864b
André Luiz Caetano <68282398+Andrelpac@users.noreply.github.com>
2021-07-26 12:26:18 -0300

    Modificando

--- src/main/java/api/teste/Testando/config/KafkaTopicConfig.java
+++ src/main/java/api/teste/Testando/config/KafkaTopicConfig.java
@@ -1,27 +1,27 @@
-//package api.teste.Testando.config;
-//
-//import org.apache.kafka.clients.admin.NewTopic;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.config.TopicBuilder;
-//
-//@Configuration
-//public class KafkaTopicConfig {
-//	
-//	@Bean
-//	public NewTopic topic1() {
-//		return TopicBuilder.name("reflectoring-1").build();
-//	}
-//	
-//	@Bean
-//	public NewTopic topic2() {
-//		return TopicBuilder.name("reflectoring-2").build();
-//	}
-//	
-//	@Value("${app.output-kafka-topic-company}")
-//	private String outputKafkaTopicCompany;
-//	
-//	@Value("${app.output-kafka-topic-pack}")
-//	private String outputKafkaTopicPack;
-//}
+package api.teste.Testando.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+	
+	@Bean
+	public NewTopic topic1() {
+		return TopicBuilder.name("reflectoring-1").build();
+	}
+	
+	@Bean
+	public NewTopic topic2() {
+		return TopicBuilder.name("reflectoring-2").build();
+	}
+	
+	@Value("${app.output-kafka-topic-company}")
+	private String outputKafkaTopicCompany;
+	
+	@Value("${app.output-kafka-topic-pack}")
+	private String outputKafkaTopicPack;
+}
