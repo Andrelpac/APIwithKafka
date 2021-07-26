57d7168e7e06218952e2bca012d1961d4ff858c5
André Luiz Caetano <68282398+Andrelpac@users.noreply.github.com>
2021-07-26 12:23:19 -0300

    Modificando

--- src/main/java/api/teste/Testando/service/KafkaConsumerService.java
+++ src/main/java/api/teste/Testando/service/KafkaConsumerService.java
@@ -16,30 +16,6 @@ public class KafkaConsumerService {
 	
 	@KafkaListener(topics = "nome_do_topico", groupId = "group_id", containerFactory = "kafkaListenerContainerFactory")
 	public void consumer(String message) throws IOException, JSONException {
-		//Cria um Objeto JSON
-		JSONObject jsonObject = new JSONObject();
-
-		FileWriter writeFile = null;
-
-		//Armazena dados em um Objeto JSON
-		jsonObject.put("messagem", message);
-
-		System.out.println("Mensagem consumida:" + message);
-		Date data = new Date();
-		SimpleDateFormat formatador = new SimpleDateFormat("ddMMyyyyHHmmss");
-		String titulo = formatador.format(data);
-		try{
-			writeFile = new FileWriter("C:\\Users\\andre\\Desktop\\Best2Bee\\Banco\\" + titulo + ".json");
-			//Escreve no arquivo conteudo do Objeto JSON
-			writeFile.write("["+jsonObject.toString()+"]");
-			writeFile.close();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-
-		//Imprimne na Tela o Objeto JSON para vizualização
-		System.out.println(jsonObject);
-		System.out.println("Arquivo " + titulo + " criado");
+		System.out.println(message);
 	}
 }
