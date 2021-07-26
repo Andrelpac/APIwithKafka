53e7c35d8516c64a7d720af3a9abec4aba07f3bf
André Luiz Caetano <68282398+Andrelpac@users.noreply.github.com>
2021-07-26 12:24:20 -0300

    Modificando

--- src/main/java/api/teste/Testando/config/KafkaConfig.java
+++ src/main/java/api/teste/Testando/config/KafkaConfig.java
@@ -1,21 +1,21 @@
-//package api.teste.Testando.config;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@ConfigurationProperties("app")
-//public class KafkaConfig {
-//
-//		private String outputKafkaTopic;
-//
-//		
-//		public String getOutputKafkaTopic() {
-//			return outputKafkaTopic;
-//		}
-//
-//		public void setOutputKafkaTopic(String outputKafkaTopic) {
-//			this.outputKafkaTopic = outputKafkaTopic;
-//		}
-//}
+package api.teste.Testando.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("app")
+public class KafkaConfig {
+
+		private String outputKafkaTopic;
+
+		
+		public String getOutputKafkaTopic() {
+			return outputKafkaTopic;
+		}
+
+		public void setOutputKafkaTopic(String outputKafkaTopic) {
+			this.outputKafkaTopic = outputKafkaTopic;
+		}
+}
 
